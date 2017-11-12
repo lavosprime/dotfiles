96c7f37dbb754d4c1cb828a1701f6d602497d2b6
Cooper Johnson <cooperdj@cs.washington.edu>
2017-11-11 19:45:26 -0800

    Make init.sh compatible with LAVOSPRIME_CONTEXT

--- bashrc/init.sh
+++ bashrc/init.sh
@@ -9,13 +9,17 @@ else
   echo -e '\e[0;31m'"  BASHRC_ROOT_DIR=${BASHRC_ROOT_DIR}"'\e[0m'
 fi
 
-BASHRC_LPENV_DIR="$BASHRC_ROOT_DIR/${LAVOSPRIME_ENVIRONMENT}.d"
+if [ ! -z "${LAVOSPRIME_CONTEXT}" ]; then
+  LAVOSPRIME_ENVIRONMENT="${LAVOSPRIME_CONTEXT}"
+fi
+
+BASHRC_LPCTX_DIR="$BASHRC_ROOT_DIR/${LAVOSPRIME_ENVIRONMENT}.d"
 if [ -z "${LAVOSPRIME_ENVIRONMENT}" ]; then
   echo 'LAVOSPRIME_ENVIRONMENT is not set!'
-elif [ -d "${BASHRC_LPENV_DIR}" ]; then
-  for f in "${BASHRC_LPENV_DIR}"/*; do source $f; done
+elif [ -d "${BASHRC_LPCTX_DIR}" ]; then
+  for f in "${BASHRC_LPCTX_DIR}"/*; do source $f; done
 else
-  echo -e '\e[0;31m'"${BASHRC_LPENV_DIR} does not exist!"'\e[0m'
+  echo -e '\e[0;31m'"${BASHRC_LPCTX_DIR} does not exist!"'\e[0m'
   echo -e '\e[0;31m'"  BASHRC_ROOT_DIR=${BASHRC_ROOT_DIR}"'\e[0m'
   echo -e '\e[0;31m'"  LAVOSPRIME_ENVIRONMENT=${LAVOSPRIME_ENVIRONMENT}"'\e[0m'
 fi
