bebb622420a978d13deaeb52895d6315a7ce2c83
Cooper Johnson <cooperdj@cs.washington.edu>
2017-11-28 23:03:33 -0800

    Switch which env vars are used for setup

--- bashrc/init.sh
+++ bashrc/init.sh
@@ -9,17 +9,17 @@ else
   echo -e '\e[0;31m'"  BASHRC_ROOT_DIR=${BASHRC_ROOT_DIR}"'\e[0m'
 fi
 
-if [ ! -z "${LAVOSPRIME_CONTEXT}" ]; then
-  LAVOSPRIME_ENVIRONMENT="${LAVOSPRIME_CONTEXT}"
+if [ ! -z "${LAVOSPRIME_ENVIRONMENT}" ]; then
+  LAVOSPRIME_CONTEXT="${LAVOSPRIME_ENVIRONMENT}"
 fi
 
-BASHRC_LPCTX_DIR="$BASHRC_ROOT_DIR/${LAVOSPRIME_ENVIRONMENT}.d"
-if [ -z "${LAVOSPRIME_ENVIRONMENT}" ]; then
-  echo 'LAVOSPRIME_ENVIRONMENT is not set!'
+BASHRC_LPCTX_DIR="$BASHRC_ROOT_DIR/${LAVOSPRIME_CONTEXT}.d"
+if [ -z "${LAVOSPRIME_CONTEXT}" ]; then
+  echo 'LAVOSPRIME_CONTEXT is not set!'
 elif [ -d "${BASHRC_LPCTX_DIR}" ]; then
   for f in "${BASHRC_LPCTX_DIR}"/*; do source $f; done
 else
   echo -e '\e[0;31m'"${BASHRC_LPCTX_DIR} does not exist!"'\e[0m'
   echo -e '\e[0;31m'"  BASHRC_ROOT_DIR=${BASHRC_ROOT_DIR}"'\e[0m'
-  echo -e '\e[0;31m'"  LAVOSPRIME_ENVIRONMENT=${LAVOSPRIME_ENVIRONMENT}"'\e[0m'
+  echo -e '\e[0;31m'"  LAVOSPRIME_CONTEXT=${LAVOSPRIME_CONTEXT}"'\e[0m'
 fi
